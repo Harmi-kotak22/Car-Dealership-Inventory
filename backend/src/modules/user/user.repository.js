82cc62063de0e83921be33e2cac6cda57b6501b2
harmiKotak <23ceuog074@ddu.ac.in>
2026-07-11 09:55:15 +0530

    Removed certain errors

--- backend/src/modules/user/user.repository.js
+++ backend/src/modules/user/user.repository.js
@@ -1,13 +1,1 @@
-const User = require('./user.model').default;
 
-class UserRepository {
-  async create(userData) {
-    return User.create(userData);
-  }
-
-  async findByEmail(email) {
-    return User.findOne({ email });
-  }
-}
-
-module.exports = new UserRepository();
